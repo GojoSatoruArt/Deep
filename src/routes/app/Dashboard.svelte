1349295c969d4cf24b96bc3ceafe6b2b2cd84073
GojoSatoruArt <gsatoru.art@gmail.com>
2025-05-21 15:27:03 +0800

    ui update

--- src/routes/app/Dashboard.svelte
+++ src/routes/app/Dashboard.svelte
@@ -1,7 +1,10 @@
 <script>
     import brandlogo from '../../../static/icons/brandlogo.svg?raw'
     import Button from '$lib/components/button.svelte';
+    import Container from '$lib/components/container.svelte';
 </script>
+
+<Container>
 
 <div class="feature flex flex-col justify-between items-start w-full h-auto bg-(--main-cardbg) rounded-2xl  border-(--main-stroke) p-5 gap-15">
 
@@ -57,24 +60,18 @@
         <div class="flex flex-col justify-center items-start gap-1">   
             <p class="opacity-75">Recommended coins for 24 Hours</p>
             <p class="font-bold text-[1.5rem]">Top Staking Assets</p>
-        
         </div>
     </div>
 
     <div>
-        
         <Button
         variant = 'secondary'
-        name = 'Hide Feature'
+        name = '24 Hours'
         />
     </div>    
-    
 </div>
 
-
-
-
-
+</Container>
 
 <style>
     .feature{
